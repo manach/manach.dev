8f2290487999fe4d976d0747a9ebe531d0578ed6
Chris Scott <chris.scott22@gmail.com>
2022-10-27 11:30:34 -0500

    more hero tweaks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      sm: "500px",
-    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
